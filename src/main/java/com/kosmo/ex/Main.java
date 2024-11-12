7e1f0eac1f2f38154afe1cc0996bd09a504288e2
0101 <jessica030327@gmail.com>
2024-11-12 10:42:15 +0900

    변경3

--- src/main/java/com/kosmo/ex/Main.java
+++ src/main/java/com/kosmo/ex/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요 !!! ");
         System.out.println("ㅎㅎㅎ;ㅎ;;;");
+        System.out.println("이건 투은지 유니버스");
     }
 }
